330d10b3dae640e03c86bb69d4a0d0cd215b4499
AdrianMehring <adriankowal1@gmail.com>
2021-06-11 10:41:39 +0200

    Rozbudowanie generatora, zaookrąglenie liczb

--- src/Autohandel/Player.java
+++ src/Autohandel/Player.java
@@ -1,11 +1,8 @@
 package Autohandel;
 import java.util.ArrayList;
 public class Player {
-    final Double money;
-    ArrayList<Vehicle> Cars = new ArrayList<>(); //lista autek
-
-
-    public Player(Double money) {
+    final long money;
+    public Player(long money) {
         this.money = money;
     }
 }
